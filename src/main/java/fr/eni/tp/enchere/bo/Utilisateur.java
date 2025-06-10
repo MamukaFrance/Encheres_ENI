d27de03eaa1e035b6d6ccee82c206770a63ce5ee
Mamuka Bagatelia <mamuka.bagatelia2025@campus-eni.fr>
2025-06-10 14:29:00 +0200

    ver1.0

--- src/main/java/fr/eni/tp/enchere/bo/Utilisateur.java
+++ src/main/java/fr/eni/tp/enchere/bo/Utilisateur.java
@@ -10,4 +10,88 @@ public class Utilisateur {
     private int credit;
     private boolean admin;
     private Adresse adresse;
+
+    public Utilisateur() {
+    }
+
+    public Utilisateur(String pseudo, String nom, String prenom, String email, String telephone, int credit, String motDePasse, boolean admin, Adresse adresse) {
+        this.pseudo = pseudo;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.telephone = telephone;
+        this.credit = credit;
+        this.motDePasse = motDePasse;
+        this.admin = admin;
+        this.adresse = adresse;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    public Adresse getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
+
 }
