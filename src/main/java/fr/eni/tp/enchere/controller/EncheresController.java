f63c17f04f16aba708c7368d630f294007959541
Samos <vil1.nazgul@gmail.com>
2025-06-11 15:10:16 +0200

    init html

---
+++ src/main/java/fr/eni/tp/enchere/controller/EncheresController.java
@@ -0,0 +1,45 @@
+package fr.eni.tp.enchere.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class EncheresController {
+//    private final EncheresService encheresService;
+//
+//    @Autowired
+//    public EncheresController(EncheresService encheresService) {
+//        this.encheresService = encheresService;
+//    }
+
+    @GetMapping
+    public String consulterEncheres() {
+        return "view-listeEncheres";
+    }
+
+    @GetMapping("/enchere")
+    public String voirEnchere() {
+        return "view-enchere";
+    }
+
+    @GetMapping("/nouvelle-vente")
+    public String nouvelleVente() {
+        return "view-nouvelleVente";
+    }
+
+    @GetMapping("/vente-remportee")
+    public String venteRemportee() {
+        return "view-venteRemportee";
+    }
+
+    @GetMapping("detail-vente")
+    public String detailVente() {
+        return "view-detailVente";
+    }
+
+    @GetMapping("ajouter-photo")
+    public String ajouterPhoto() {
+        return "view-ajouterPhoto";
+    }
+}
