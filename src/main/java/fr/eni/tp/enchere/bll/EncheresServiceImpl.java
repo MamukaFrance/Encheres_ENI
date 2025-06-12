4bde54446a476ae89308169f0746c2a2d5ddbdc1
Benjamin Cft <benjamin.coeffeteau2025@campus-eni.fr>
2025-06-12 16:14:13 +0200

    Maj EncheresServiceImpl : nouvellevente, getadresses, getcategories

--- src/main/java/fr/eni/tp/enchere/bll/EncheresServiceImpl.java
+++ src/main/java/fr/eni/tp/enchere/bll/EncheresServiceImpl.java
@@ -6,6 +6,7 @@ import fr.eni.tp.enchere.bo.Categorie;
 import fr.eni.tp.enchere.bo.Enchere;
 import fr.eni.tp.enchere.dal.AdresseDAO;
 import fr.eni.tp.enchere.dal.ArticleAVendreDAO;
+import fr.eni.tp.enchere.dal.CategorieDAO;
 import fr.eni.tp.enchere.dal.UtilisateursDAO;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
@@ -16,13 +17,15 @@ import java.util.stream.Collectors;
 
 @Service
 @Primary
-public class EncheresServiceImpl implements EncheresService{
+public class EncheresServiceImpl implements EncheresService {
 
+    private CategorieDAO categorieDAO;
     private AdresseDAO adresseDAO;
-private ArticleAVendreDAO articleAVendreDAO;
-private UtilisateursDAO utilisateursDAO;
+    private ArticleAVendreDAO articleAVendreDAO;
+    private UtilisateursDAO utilisateursDAO;
 
-    public EncheresServiceImpl(AdresseDAO adresseDAO, ArticleAVendreDAO articleAVendreDAO, UtilisateursDAO utilisateursDAO) {
+    public EncheresServiceImpl(CategorieDAO categorieDAO, AdresseDAO adresseDAO, ArticleAVendreDAO articleAVendreDAO, UtilisateursDAO utilisateursDAO) {
+        this.categorieDAO = categorieDAO;
         this.adresseDAO = adresseDAO;
         this.articleAVendreDAO = articleAVendreDAO;
         this.utilisateursDAO = utilisateursDAO;
@@ -31,7 +34,7 @@ private UtilisateursDAO utilisateursDAO;
     @Override
     // Sur la page d'accueil on demande la liste des encheres en cours, mais on veut en
     // faite la liste des objets a vendre!!(consulter enchere mais on affiche une liste d'objet a vendre)
-    public List<ArticleAVendre>  consulterEncheres() {
+    public List<ArticleAVendre> consulterEncheres() {
         List<ArticleAVendre> articlesAVendres = this.articleAVendreDAO.readAll();
 
         LocalDate aujourdHui = LocalDate.now();
@@ -46,22 +49,22 @@ private UtilisateursDAO utilisateursDAO;
 
     @Override
     public ArticleAVendre voirEnchere(Long id) {
-        return null;
+        return articleAVendreDAO.read(id);
     }
 
     @Override
     public void nouvelleVente(ArticleAVendre articleAvendre) {
-
+        articleAVendreDAO.create(articleAvendre);
     }
 
     @Override
     public List<Adresse> getAdresses() {
-        return List.of();
+        return adresseDAO.readAll();
     }
 
     @Override
     public List<Categorie> getCategories() {
-        return List.of();
+        return categorieDAO.readAll();
     }
 
     @Override
